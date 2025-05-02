81e901a47df34294c150758a94e9408f17b4d8a7
oscar <aaronjones2429@gmail.com>
2025-05-02 13:14:15 -0500

    subiendo Factuta y servicio

--- Proyecto_CarsMotors/src/com/carsmotors/model/Servicio.java
+++ Proyecto_CarsMotors/src/com/carsmotors/model/Servicio.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.carsmotors.model;
 
+import java.sql.Time;
+
 /**
- *
- * @author oscarcampus
+ * Clase que reprecenta un tipo de servicio ofrecido por el taller
  */
 public class Servicio {
+    private int id;
+    private String descripcion;
+    private String tipo;
+    private double costoManoObra;
+    private Time duracionEstimada;
     
+    // Constructor vacío
+    public Servicio() {
+    }
+    
+    // Constructor completo
+    public Servicio(int id, String descripcion, String tipo, double costoManoObra, Time duracionEstimada) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.costoManoObra = costoManoObra;
+        this.duracionEstimada = duracionEstimada;
+    }
+    
+    // Constructor sin ID para nuevos servicios
+    public Servicio(String descripcion, String tipo, double costoManoObra, Time duracionEstimada) {
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.costoManoObra = costoManoObra;
+        this.duracionEstimada = duracionEstimada;
+    }
+    
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public double getCostoManoObra() {
+        return costoManoObra;
+    }
+    
+    public void setCostoManoObra(double costoManoObra) {
+        this.costoManoObra = costoManoObra;
+    }
+    
+    public Time getDuracionEstimada() {
+        return duracionEstimada;
+    }
+    
+    public void setDuracionEstimada(Time duracionEstimada) {
+        this.duracionEstimada = duracionEstimada;
+    }
+    
+    @Override
+    public String toString() {
+        return descripcion + " - " + tipo;
+    }
 }
