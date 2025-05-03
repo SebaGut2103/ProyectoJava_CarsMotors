108f03c354e5a0dc77aee296b849a79c0e7469cd
oscar <aaronjones2429@gmail.com>
2025-05-02 19:11:44 -0500

    subiendo cambiso

--- Proyecto_CarsMotors/src/com/carsmotors/database/DatabaseConnection.java
+++ Proyecto_CarsMotors/src/com/carsmotors/database/DatabaseConnection.java
@@ -15,7 +15,7 @@ public class DatabaseConnection {
     // configuracion Inicial  de la Base de datos
     private static final String URL = "jdbc:mysql://localhost:3306/taller_automotriz";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "root";
     
     private DatabaseConnection() {
         try {
